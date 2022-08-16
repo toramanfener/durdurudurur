7a3c99edd752d887265e4548b27cf2f3e8a4a703
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-16 19:31:50 +0300

    Add files via upload

--- kelime_bot/plugins/stop.py
+++ kelime_bot/plugins/stop.py
@@ -9,18 +9,18 @@ from kelime_bot import *
 
 
 
-@Client.on_message(filters.command("cancel") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("stop") & ~filters.private & ~filters.channel)
 async def stop(c:Client, m:Message):
     global oyun
     
     siralama = []
     for i in oyun[m.chat.id]["oyuncular"]:
-        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} Bal")
+        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} 𝖯𝗎𝖺𝗇")
     siralama.sort(reverse=True)
     siralama_text = ""
     for i in siralama:
         siralama_text += i + "\n"     
     
-    await c.send_message(m.chat.id, f"**{m.from_user.mention}** Tarafından Oyun Bitirildi\n\nYeni Oyuna Başlamak İçin /game Yaza Bilirsiniz\n\n 📝 Puan Listesi  :\n\n{siralama_text}")
+    await c.send_message(m.chat.id, f"**{m.from_user.mention}** 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 ! \n 𝖮𝗒𝗎𝗇 𝖡𝗂𝗍𝗍𝗂𝗋𝗂𝗅𝖽𝗂 ! \n 𝖸𝖾𝗇𝗂 𝖮𝗒𝗎𝗇 𝖡𝖺𝗌𝗅𝖺𝗍𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 \n /game 𝖸𝖺𝗓𝖺𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 . . .\n\n 📝 𝖯𝗎𝖺𝗇 𝖳𝖺𝖻𝗅𝗈𝗌𝗎  :\n\n{siralama_text}")
     oyun[m.chat.id] = {}
     
