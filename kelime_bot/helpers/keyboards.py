68ac21d2aeddc1ce66f8959907537ff6befe865c
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-16 20:35:14 +0300

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -2,6 +2,6 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("📝  ᴅᴜʏᴜʀᴜ ᴋᴀɴᴀʟɪ  " , url= "t.me/StarBotKanal")]
+    [InlineKeyboardButton("📝  İtiraf ve Eğlence Kanalımız " , url= "t.me/eglencebotlari")]
 ])
 
