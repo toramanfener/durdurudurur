6099ac8bcb84889aca0cc74946ee344e0d481662
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-19 00:46:03 +0300

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -17,7 +17,7 @@ async def ratingsa(c:Client, m:Message):
         puanlar.append(rating[kisi])
     puanlar.sort(reverse = True)
     for puanlar in list:
-    for puan in puanlar:
+        for puan in puanlar:
         for kisi in rating:
             if puan == rating[kisi]:
                 metin += f"**{kisi}** :  {puan}  𝖯𝗎𝖺𝗇\n"
