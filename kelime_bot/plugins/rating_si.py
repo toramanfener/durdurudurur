504e23275494923dd81e4af9007502bad5bc9d26
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-19 01:13:15 +0300

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -22,7 +22,7 @@ async def ratingsa(c:Client, m:Message):
                 metin += f"**{kisi}** : {puan}  Puan\n"
                 eklenen += 1
                 if eklenen == 20:
-                    for puan in list:
+                    for metin in list:
                         print(1)
                     break
                 
