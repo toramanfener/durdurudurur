b3bc735163df4e9c1e15c18a4d069318fe9c1d93
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-19 02:01:08 +0300

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -22,8 +22,8 @@ async def ratingsa(c:Client, m:Message):
                 metin += f"**{kisi}** : {puan}  Puan\n"
                 eklenen += 1
                 if eklenen == 20:
-                    for skor in range (1, 20):
-                        print(skor)
+                    for ratingsa in range (1, 20):
+                        print(ratingsa)
                     break
                 
     await c.send_message(m.chat.id, metin)
