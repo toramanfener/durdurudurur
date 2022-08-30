f803096b1a5882aba16b5b1a0ec782f57900447b
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-30 20:33:31 +0300

    Delete engine_factory.py

--- kelime_bot/plugins/engine_factory.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-
-def generate_engine(DB_HOST: str, DB_PORT: str, DB_NAME: str, DB_USER: str, DB_PASSWORD: str):
-    return create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
-                            echo=False)
