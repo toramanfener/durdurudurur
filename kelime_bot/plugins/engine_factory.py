4bcbb6c59d756b7b163bb21e6eac90deaaf89ae3
Deniz <46014377+toramanfener@users.noreply.github.com>
2022-08-30 20:26:24 +0300

    Create engine_factory.py

---
+++ kelime_bot/plugins/engine_factory.py
@@ -0,0 +1,5 @@
+from sqlalchemy import 
+
+def generate_engine(DB_HOST: str, DB_PORT: str, DB_NAME: str, DB_USER: str, DB_PASSWORD: str):
+    return create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
+                            echo=False)
